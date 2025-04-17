7e01e645c338c7f5bff457968b440b0d59a2bbb3
ketkar-atharva <atharvaketkar13@gmail.com>
2025-04-17 23:40:04 +0530

    added prep exam container

--- todo.js
+++ todo.js
@@ -1,5 +1,8 @@
 let input=document.getElementById('input-box');
 let list=document.getElementById('listcontainer');
+let list1=document.getElementById('listcontainer1');
+ let input1=document.getElementById('input-box1');
+ let p=document.getElementById('prep');
 let t=document.getElementById('todo');
 let n=1;
 function additem(){
@@ -27,4 +30,31 @@ list.addEventListener('click',function(e){
    }else if( e.target.tagName=="SPAN"){
        e.target.parentElement.remove();
    }
+},false);
+
+function additem1(){
+  if(input1.value==""){
+    alert("Enter task");
+  }else if(n<=6){
+    let li=document.createElement("li");
+    li.innerHTML=input1.value;
+    list1.appendChild(li);
+    p.style.height="400px";
+    n++;
+    let span=document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span);
+
+
+  }else{
+    alert("Only 6 task can be entered");
+  }
+  input1.value="";
+}
+list1.addEventListener('click',function(e){
+if(e.target.tagName=="LI"){
+    e.target.classList.toggle("checked");
+}else if( e.target.tagName=="SPAN"){
+    e.target.parentElement.remove();
+}
 },false);
