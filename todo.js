44643515bceb574d77e1b1a142961e966df38146
ketkar-atharva <atharvaketkar13@gmail.com>
2025-04-18 21:32:35 +0530

    first demo completed

--- todo.js
+++ todo.js
@@ -11,13 +11,32 @@ let list2=document.getElementById('listcontainer2');
 let title=document.getElementById('input-boxt');
 let add=document.getElementById('addtask');
 let addtitle=document.getElementById('addtitle');
+let h1=document.getElementById('head1');
+let h2=document.getElementById('head2');
+let title1=document.getElementById('input-boxt1');
+let title2=document.getElementById('input-boxt2');
+let addtitle1=document.getElementById('addtitle1');
+let addtitle2=document.getElementById('addtitle2');
+h1.style.display="none";
+let add1=document.getElementById('addtask0');
+let add2=document.getElementById('addtask2');
 add.style.display="none";
+add1.style.display="none";
+add2.style.display="none";
 h.style.display="none";
-
+h2.style.display="none";
+let ev1=document.getElementById('container1');
+let ev2=document.getElementById('container2');
+let ev3=document.getElementById('container3');
+ev1.style.display="none";
+ev2.style.display="none";
+ev3.style.display="none";
 
 
 
 let n=1;
+let m=1;
+let v=1;
 function additem(){
      if(input.value==""){
        alert("Enter task");
@@ -49,12 +68,12 @@ list.addEventListener('click',function(e){
 function additem1(){
    if(input1.value==""){
      alert("Enter task");
-   }else if(n<=6){
+   }else if(m<=6){
      let li=document.createElement("li");
      li.innerHTML=input1.value;
      list1.appendChild(li);
      p.style.height="400px";
-     n++;
+     m++;
      let span=document.createElement("span");
      span.innerHTML="\u00d7";
      li.appendChild(span);
@@ -70,7 +89,7 @@ list1.addEventListener('click',function(e){
      e.target.classList.toggle("checked");
  }else if( e.target.tagName=="SPAN"){
      e.target.parentElement.remove();
-     n--;
+     m--;
  }
 },false);
 
@@ -87,15 +106,41 @@ function additemt(){
  }
  input2.value="";
 }
+function additemt1(){
+ if(title1.value==""){
+   alert("Add title");
+ }else{
+   let t=document.createElement("h3");
+   t.innerHTML=title1.value;
+   h1.appendChild(t);
+   addtitle1.style.display="none";
+   h1.style.display="block";
+  add1.style.display="inline-flex";
+ }
+ input2.value="";
+}
+function additemt2(){
+ if(title2.value==""){
+   alert("Add title");
+ }else{
+   let t=document.createElement("h3");
+   t.innerHTML=title2.value;
+   h2.appendChild(t);
+   addtitle2.style.display="none";
+   h2.style.display="block";
+  add2.style.display="inline-flex";
+ }
+ input2.value="";
+}
 function additem2(){
  if(input2.value==""){
    alert("Enter task");
- }else if(n<=6){
+ }else if(v<=6){
    let li=document.createElement("li");
    li.innerHTML=input2.value;
    list2.appendChild(li);
    f.style.height="400px";
-   n++;
+   v++;
    let span=document.createElement("span");
    span.innerHTML="\u00d7";
    li.appendChild(span);
@@ -111,6 +156,22 @@ list2.addEventListener('click',function(e){
      e.target.classList.toggle("checked");
  }else if( e.target.tagName=="SPAN"){
      e.target.parentElement.remove();
-     n--;
+     v--;
  }
 },false);
+
+let k=1;
+function addevent(){
+ if(k==1){
+   ev1.style.display="flex";
+   k++;
+ }else if(k==2){
+   ev2.style.display="flex";
+   k++;
+ }else if(k==3){
+   ev3.style.display="flex";
+   k++;
+ }else{
+   alert("Only 3 events can be added");
+ }
+}
